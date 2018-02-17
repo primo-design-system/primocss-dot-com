37c3876db7d4e8a8e6aa59ca95f3ebf9da87e388
Richard McCartney <richardmccartney@monzo.com>
2018-02-17 16:01:44 +0000

    updating package json and server js.

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:8000
 app.use(express.static('public'));
 
-app.listen(8080);
+app.listen(8000);
 
-console.log("Running at Port 8080 http://localhost:8080");
+console.log("Running at Port 8000 http://localhost:8080");
